9c5cb700b42554502939b1e447de012b8e5878cf
nokazn <nokazn@gmail.com>
2020-08-06 22:54:27 +0900

    refactor

--- mkswap.sh
+++ mkswap.sh
@@ -1,32 +1,32 @@
-# !/bin/bash
+#!/bin/bash
 
 SWAPFILE=/swapfile
-MEMSIZE=`cat /proc/meminfo | grep MemTotal | awk '{print $2}'`
+MEMSIZE=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
 
-if [ $MEMSIZE -lt 2097152 ]; then
-  SIZE=$((MEMSIZE * 2))k
-elif [ $MEMSIZE -lt 8388608 ]; then
-  SIZE=${MEMSIZE}k
-elif [ $MEMSIZE -lt 67108864]; then
-  SIZE=$((MEMSIZE  / 2))k
+if [[ ${MEMSIZE} -lt 2097152 ]]; then
+  SIZE="$((${MEMSIZE} * 2))k"
+elif [[ ${MEMSIZE} -lt 8388608 ]]; then
+  SIZE="${MEMSIZE}k"
+elif [ ${MEMSIZE} -lt 67108864]; then
+  SIZE="$((${MEMSIZE}  / 2))k"
 else
-  SIZE=4194304k
+  SIZE="4194304k"
 fi
 
-if [ -e $SWAPFILE ]; then
-  CURRENTSIZE=`du $SWAPFILE | awk '{print $1}'`
+if [[ -e ${SWAPFILE} ]]; then
+  CURRENTSIZE=$(du ${SWAPFILE} | awk '{print $1}')
 else
   CURRENTSIZE=0
 fi
 
-if [ $SIZE -ne $CURRENTSIZE ]; then
-  dd if=/dev/zero out=$SWAPFILE count $SIZE bs=1k
-  chmod 600 $SWAPFILE
-  mkswap $SWAPFILE
-  swapon $SWAPFILE
+if [[ ${SIZE} -ne ${CURRENTSIZE} ]]; then
+  dd if=/dev/zero out=${SWAPFILE} count ${SIZE} bs=1k
+  chmod 600 ${SWAPFILE}
+  mkswap ${SWAPFILE}
+  swapon ${SWAPFILE}
 else
-  swapon $SWAPFILE
+  swapon ${SWAPFILE}
 fi
 
-echo $MEMSIZE
-echo $(($MEMSIZE * 2))
+echo ${MEMSIZE}
+echo $((${MEMSIZE} * 2))
